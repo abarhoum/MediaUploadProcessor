ce7cb8c59f849d4a72f74ff5c88a6544da084619
abarhoum <abarhoum84@gmail.com>
2025-09-16 16:33:37 +0400

    first push

---
+++ Sitecore.Foundation.CustomMediaUploadProcessor/AfterUploadProcessor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Sitecore.Data.Items;
+using Sitecore.Pipelines.Upload;
+using Sitecore.SecurityModel;
+
+namespace Sitecore.Foundation.CustomMediaUploadProcessor
+{
+    public class AfterUploadProcessor
+    {
+        public void Process(UploadArgs args)
+        {
+            if (args == null || args.Files == null || args.Files.Count == 0)
+                return;
+
+            // Read the date-only value you added to the upload dialog (input name="UploadDate")
+            var dateStr = System.Web.HttpContext.Current?.Request?["UploadDate"];
+            DateTime dateOnly;
+            var hasDate = DateTime.TryParse(dateStr, out dateOnly);
+
+            if (args.UploadedItems == null || args.UploadedItems.Count == 0)
+                return; 
+            var uploadedItem = args.UploadedItems[0];
+
+            string isoDate = dateOnly.ToString("yyyyMMddTHHmmss");
+
+            using (new SecurityDisabler())
+            {
+                uploadedItem.Editing.BeginEdit();
+                try
+                {
+                    // replace "Event Date" with your actual field name or ID
+                    uploadedItem.Fields["File Date"].Value = isoDate;
+                    uploadedItem.Editing.EndEdit();
+                }
+                catch
+                {
+                    uploadedItem.Editing.CancelEdit();
+                    throw;
+                }
+            }
+
+        }
+    }
+}
---
+++ Sitecore.Foundation.CustomMediaUploadProcessor/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sitecore.Foundation.CustomMediaUploadProcessor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sitecore.Foundation.CustomMediaUploadProcessor")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3e320fbf-9988-4fcd-99c7-cc6debe1758a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
